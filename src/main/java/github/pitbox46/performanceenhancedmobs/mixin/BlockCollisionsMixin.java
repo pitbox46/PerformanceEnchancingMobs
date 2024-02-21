8cfe7cfc633db4ad85fcc02b7fe531071e6dc1f9
pitbox46 <pitbox46@gmail.com>
2024-02-21 13:18:56 -0500

    Caching equitable to regular performance

--- src/main/java/github/pitbox46/performanceenhancedmobs/mixin/BlockCollisionsMixin.java
+++ src/main/java/github/pitbox46/performanceenhancedmobs/mixin/BlockCollisionsMixin.java
@@ -44,63 +44,63 @@ public abstract class BlockCollisionsMixin<T> extends AbstractIterator<T> {
      * @author pitbox46
      * @reason More performant than using a redirect and an Injection is not feasible due to no implementation for *continue*
      */
-//    @Overwrite
-//    protected T computeNext() {
-//        while(true) {
-//            if (this.cursor.advance()) {
-//                int i = this.cursor.nextX();
-//                int j = this.cursor.nextY();
-//                int k = this.cursor.nextZ();
-//                int l = this.cursor.getNextType();
-//                if (l == 3) {
-//                    continue;
-//                }
-//
-//                BlockGetter blockgetter = this.getChunk(i, k);
-//                if (blockgetter == null) {
-//                    continue;
-//                }
-//
-//                this.pos.set(i, j, k);
-//                BlockState blockstate = blockgetter.getBlockState(this.pos);
-//                if (this.onlySuffocatingBlocks && !blockstate.isSuffocating(blockgetter, this.pos) || l == 1 && !blockstate.hasLargeCollisionShape() || l == 2 && !blockstate.is(Blocks.MOVING_PISTON)) {
-//                    continue;
-//                }
-//
-//                VoxelShape voxelshape = blockstate.getCollisionShape(this.collisionGetter, this.pos, this.context);
-//                if (voxelshape == Shapes.block()) {
-//                    if (!this.box.intersects(i, j, k, i + 1.0D, j + 1.0D, k + 1.0D)) {
-//                        continue;
-//                    }
-//
-//                    return this.resultProvider.apply(this.pos, voxelshape.move(i, j, k));
-//                }
-//
-//                VoxelShape voxelshape1 = voxelshape.move(i, j, k);
-//
-//                if (voxelshape1.isEmpty()) {
-//                    continue;
-//                }
-//
-//                boolean flag;
-//                if (collisionGetter instanceof LevelDuck duck) {
-//                    var cacheMap = duck.performanceEnhancedMobs$getBlockCollisionCacheMap();
-//                    Pair<VoxelShape, VoxelShape> pair = Pair.of(voxelshape1, this.entityShape);
-//                    if (!cacheMap.containsKey(pair)) {
-//                        cacheMap.put(pair, Shapes.joinIsNotEmpty(voxelshape1, this.entityShape, BooleanOp.AND));
-//                    }
-//                    flag = cacheMap.get(pair);
-//                } else {
-//                    flag = Shapes.joinIsNotEmpty(voxelshape1, this.entityShape, BooleanOp.AND);
-//                }
-//                if (!flag) {
-//                    continue;
-//                }
-//
-//                return this.resultProvider.apply(this.pos, voxelshape1);
-//            }
-//
-//            return this.endOfData();
-//        }
-//    }
+    @Overwrite
+    protected T computeNext() {
+        while(true) {
+            if (this.cursor.advance()) {
+                int i = this.cursor.nextX();
+                int j = this.cursor.nextY();
+                int k = this.cursor.nextZ();
+                int l = this.cursor.getNextType();
+                if (l == 3) {
+                    continue;
+                }
+
+                BlockGetter blockgetter = this.getChunk(i, k);
+                if (blockgetter == null) {
+                    continue;
+                }
+
+                this.pos.set(i, j, k);
+                BlockState blockstate = blockgetter.getBlockState(this.pos);
+                if (this.onlySuffocatingBlocks && !blockstate.isSuffocating(blockgetter, this.pos) || l == 1 && !blockstate.hasLargeCollisionShape() || l == 2 && !blockstate.is(Blocks.MOVING_PISTON)) {
+                    continue;
+                }
+
+                VoxelShape voxelshape = blockstate.getCollisionShape(this.collisionGetter, this.pos, this.context);
+                if (voxelshape == Shapes.block()) {
+                    if (!this.box.intersects(i, j, k, i + 1.0D, j + 1.0D, k + 1.0D)) {
+                        continue;
+                    }
+
+                    return this.resultProvider.apply(this.pos, voxelshape.move(i, j, k));
+                }
+
+                VoxelShape voxelshape1 = voxelshape.move(i, j, k);
+
+                if (voxelshape1.isEmpty()) {
+                    continue;
+                }
+
+                boolean flag;
+                if (collisionGetter instanceof LevelDuck duck) {
+                    var cacheMap = duck.performanceEnhancedMobs$getBlockCollisionCacheMap();
+                    Pair<VoxelShape, VoxelShape> pair = Pair.of(voxelshape1, this.entityShape);
+                    if (!cacheMap.containsKey(pair)) {
+                        cacheMap.put(pair, Shapes.joinIsNotEmpty(voxelshape1, this.entityShape, BooleanOp.AND));
+                    }
+                    flag = cacheMap.get(pair);
+                } else {
+                    flag = Shapes.joinIsNotEmpty(voxelshape1, this.entityShape, BooleanOp.AND);
+                }
+                if (!flag) {
+                    continue;
+                }
+
+                return this.resultProvider.apply(this.pos, voxelshape1);
+            }
+
+            return this.endOfData();
+        }
+    }
 }
